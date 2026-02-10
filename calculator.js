a1efd86bf657f1e80925ed40a8eb6741264ca1c3
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-02-09 22:12:38 -0800

    add: (testing) simple number entry system

--- calculator.js
+++ calculator.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("hello");
-});
+document.addEventListener("DOMContentLoaded", function () {});
+
+function numberCheck(a) {
+  const number = parseInt(a.trim(), 10);
+  if (isNaN(number)) {
+    alert("invalid input, enter a number");
+    return null;
+  } else return number;
+}
+
+function numberEntry(array) {
+  let validatedNumber = null;
+  while (validatedNumber === null) {
+    const userInput = prompt("enter number");
+    validatedNumber = numberCheck(userInput);
+  }
+  array.push(validatedNumber);
+}
