70efcea1bba97961129ad9dd1ea801178417c2ba
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-02-09 22:17:04 -0800

    add: basic math operations and logs for testing

--- calculator.js
+++ calculator.js
@@ -1,4 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {});
+document.addEventListener("DOMContentLoaded", function () {
+  const userArray = [];
+  numberEntry(userArray);
+  numberEntry(userArray);
+  console.log("add ", add(userArray[0], userArray[1]));
+  console.log("subtract ", subtract(userArray[0], userArray[1]));
+  console.log("multiply ", multiply(userArray[0], userArray[1]));
+  console.log("divide ", divide(userArray[0], userArray[1]));
+});
+
+function numberEntry(array) {
+  let validatedNumber = null;
+  while (validatedNumber === null) {
+    const userInput = prompt("enter number");
+    validatedNumber = numberCheck(userInput);
+  }
+  array.push(validatedNumber);
+}
 
 function numberCheck(a) {
   const number = parseInt(a.trim(), 10);
@@ -8,11 +25,18 @@ function numberCheck(a) {
   } else return number;
 }
 
-function numberEntry(array) {
-  let validatedNumber = null;
-  while (validatedNumber === null) {
-    const userInput = prompt("enter number");
-    validatedNumber = numberCheck(userInput);
-  }
-  array.push(validatedNumber);
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
 }
