894e5ab9cf2578dcb6dfc11e109870474b55c1dc
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-02-12 13:08:34 -0800

    add: display previous number

--- calculator.js
+++ calculator.js
@@ -1,9 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   const calculatorButtons = document.querySelectorAll("button");
-  const result = document.createElement("div");
-  result.classList.add("result");
-  result.textContent = "0";
-  container.appendChild(result);
+  const firstNumber = document.createElement("div");
+  const secondNumber = document.createElement("div");
+  firstNumber.classList.add("firstNumber");
+  secondNumber.classList.add("secondNumber");
+  firstNumber.textContent = "0";
+  secondNumber.textContent = "";
+  container.appendChild(firstNumber);
+  container.appendChild(secondNumber);
 
   let userArray = [0, 0];
   let currentArrayPosition = 0;
@@ -16,94 +20,101 @@ document.addEventListener("DOMContentLoaded", function () {
         case "zero":
           currentNumber = "0";
           enterValue(userArray, currentNumber, currentArrayPosition);
-          result.textContent = userArray[0];
+          displayFirstNumber(userArray, firstNumber);
           break;
         case "one":
           currentNumber = "1";
           enterValue(userArray, currentNumber, currentArrayPosition);
-          result.textContent = userArray[0];
+          displayFirstNumber(userArray, firstNumber);
           break;
         case "two":
           currentNumber = "2";
           enterValue(userArray, currentNumber, currentArrayPosition);
-          result.textContent = userArray[0];
+          displayFirstNumber(userArray, firstNumber);
           break;
         case "three":
           currentNumber = "3";
           enterValue(userArray, currentNumber, currentArrayPosition);
-          result.textContent = userArray[0];
+          displayFirstNumber(userArray, firstNumber);
           break;
         case "four":
           currentNumber = "4";
           enterValue(userArray, currentNumber, currentArrayPosition);
-          result.textContent = userArray[0];
+          displayFirstNumber(userArray, firstNumber);
           break;
         case "five":
           currentNumber = "5";
           enterValue(userArray, currentNumber, currentArrayPosition);
-          result.textContent = userArray[0];
+          displayFirstNumber(userArray, firstNumber);
           break;
         case "six":
           currentNumber = "6";
           enterValue(userArray, currentNumber, currentArrayPosition);
-          result.textContent = userArray[0];
+          displayFirstNumber(userArray, firstNumber);
           break;
         case "seven":
           currentNumber = "7";
           enterValue(userArray, currentNumber, currentArrayPosition);
-          result.textContent = userArray[0];
+          displayFirstNumber(userArray, firstNumber);
           break;
         case "eight":
           currentNumber = "8";
           enterValue(userArray, currentNumber, currentArrayPosition);
-          result.textContent = userArray[0];
+          displayFirstNumber(userArray, firstNumber);
           break;
         case "nine":
           currentNumber = "9";
           enterValue(userArray, currentNumber, currentArrayPosition);
-          result.textContent = userArray[0];
+          displayFirstNumber(userArray, firstNumber);
           break;
         case "add":
           currentArrayPosition = 1;
           calculate(userArray, currentOperation);
-          result.textContent = userArray[0];
+          displayFirstNumber(userArray, firstNumber);
           currentOperation = "add";
           break;
         case "subtract":
           currentArrayPosition = 1;
           calculate(userArray, currentOperation);
-          result.textContent = userArray[0];
+          displayFirstNumber(userArray, firstNumber);
           currentOperation = "subtract";
           break;
         case "multiply":
           currentArrayPosition = 1;
           calculate(userArray, currentOperation);
-          result.textContent = userArray[0];
+          displayFirstNumber(userArray, firstNumber);
           currentOperation = "multiply";
           break;
         case "divide":
           currentArrayPosition = 1;
           calculate(userArray, currentOperation);
-          result.textContent = userArray[0];
+          displayFirstNumber(userArray, firstNumber);
           currentOperation = "divide";
           break;
-        case "equals":
+        case "equal":
           currentArrayPosition = 1;
           calculate(userArray, currentOperation);
-          result.textContent = userArray[0];
+          displayFirstNumber(userArray, firstNumber);
           currentArrayPosition = 0;
           currentOperation = null;
           break;
-        case "allClear":
+        case "clear":
           userArray = [0, 0];
-          result.textContent = userArray[0];
+          displayFirstNumber(userArray, firstNumber);
           currentArrayPosition = 0;
           currentOperation = null;
           break;
       }
+      console.log(userArray);
+      console.log(currentOperation);
     });
   });
 });
+
+function displayFirstNumber(array, first) {
+  if (array[1] != 0) first.textContent = array[1];
+  else first.textContent = array[0];
+}
 
 function enterValue(array, n, position) {
   const num = parseInt(n, 10);
