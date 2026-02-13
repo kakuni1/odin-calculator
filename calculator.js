19abc51cf822a5b52d6343c16adcec2ddd1ed07e
kakuni1 <127331940+kakuni1@users.noreply.github.com>
2026-02-12 18:33:00 -0800

    add: array expanded to include result & math operation symbol

--- calculator.js
+++ calculator.js
@@ -2,17 +2,24 @@ document.addEventListener("DOMContentLoaded", function () {
   const calculatorButtons = document.querySelectorAll("button");
   const firstNumber = document.createElement("div");
   const secondNumber = document.createElement("div");
+  const resultNumber = document.createElement("div");
   firstNumber.classList.add("firstNumber");
   secondNumber.classList.add("secondNumber");
+  resultNumber.classList.add("resultNumber");
   firstNumber.textContent = "0";
   secondNumber.textContent = "";
+  resultNumber.textContent = "";
   container.appendChild(firstNumber);
   container.appendChild(secondNumber);
+  container.appendChild(resultNumber);
 
-  let userArray = [0, 0];
-  let currentArrayPosition = 0;
-  let currentOperation = null;
+  // userArray[0] : calculation result
+  // userArray[1] : 1st number slot
+  // userArray[2] : 2nd number slot
+  let userArray = [0, 0, 0, ""];
+  let currentArrayPosition = 1;
   let currentNumber = null;
+  let currentOperation = null;
 
   calculatorButtons.forEach((button) => {
     button.addEventListener("click", () => {
@@ -20,108 +27,126 @@ document.addEventListener("DOMContentLoaded", function () {
         case "zero":
           currentNumber = "0";
           enterValue(userArray, currentNumber, currentArrayPosition);
-          displayFirstNumber(userArray, firstNumber);
+          displayAll(userArray, firstNumber, secondNumber, resultNumber);
           break;
         case "one":
           currentNumber = "1";
           enterValue(userArray, currentNumber, currentArrayPosition);
-          displayFirstNumber(userArray, firstNumber);
+          displayAll(userArray, firstNumber, secondNumber, resultNumber);
           break;
         case "two":
           currentNumber = "2";
           enterValue(userArray, currentNumber, currentArrayPosition);
-          displayFirstNumber(userArray, firstNumber);
+          displayAll(userArray, firstNumber, secondNumber, resultNumber);
           break;
         case "three":
           currentNumber = "3";
           enterValue(userArray, currentNumber, currentArrayPosition);
-          displayFirstNumber(userArray, firstNumber);
+          displayAll(userArray, firstNumber, secondNumber, resultNumber);
           break;
         case "four":
           currentNumber = "4";
           enterValue(userArray, currentNumber, currentArrayPosition);
-          displayFirstNumber(userArray, firstNumber);
+          displayAll(userArray, firstNumber, secondNumber, resultNumber);
           break;
         case "five":
           currentNumber = "5";
           enterValue(userArray, currentNumber, currentArrayPosition);
-          displayFirstNumber(userArray, firstNumber);
+          displayAll(userArray, firstNumber, secondNumber, resultNumber);
           break;
         case "six":
           currentNumber = "6";
           enterValue(userArray, currentNumber, currentArrayPosition);
-          displayFirstNumber(userArray, firstNumber);
+          displayAll(userArray, firstNumber, secondNumber, resultNumber);
           break;
         case "seven":
           currentNumber = "7";
           enterValue(userArray, currentNumber, currentArrayPosition);
-          displayFirstNumber(userArray, firstNumber);
+          displayAll(userArray, firstNumber, secondNumber, resultNumber);
           break;
         case "eight":
           currentNumber = "8";
           enterValue(userArray, currentNumber, currentArrayPosition);
-          displayFirstNumber(userArray, firstNumber);
+          displayAll(userArray, firstNumber, secondNumber, resultNumber);
           break;
         case "nine":
           currentNumber = "9";
           enterValue(userArray, currentNumber, currentArrayPosition);
-          displayFirstNumber(userArray, firstNumber);
+          displayAll(userArray, firstNumber, secondNumber, resultNumber);
           break;
         case "add":
-          currentArrayPosition = 1;
+          currentArrayPosition = 2;
+          userArray[3] = "+";
           calculate(userArray, currentOperation);
-          displayFirstNumber(userArray, firstNumber);
+          displayAll(userArray, firstNumber, secondNumber, resultNumber);
           currentOperation = "add";
           break;
         case "subtract":
-          currentArrayPosition = 1;
+          currentArrayPosition = 2;
+          userArray[3] = "-";
           calculate(userArray, currentOperation);
-          displayFirstNumber(userArray, firstNumber);
+          displayAll(userArray, firstNumber, secondNumber, resultNumber);
           currentOperation = "subtract";
           break;
         case "multiply":
-          currentArrayPosition = 1;
+          currentArrayPosition = 2;
+          userArray[3] = "*";
           calculate(userArray, currentOperation);
-          displayFirstNumber(userArray, firstNumber);
+          displayAll(userArray, firstNumber, secondNumber, resultNumber);
           currentOperation = "multiply";
           break;
         case "divide":
-          currentArrayPosition = 1;
+          currentArrayPosition = 2;
+          userArray[3] = "/";
           calculate(userArray, currentOperation);
-          displayFirstNumber(userArray, firstNumber);
+          displayAll(userArray, firstNumber, secondNumber, resultNumber);
           currentOperation = "divide";
           break;
         case "equal":
-          currentArrayPosition = 1;
           calculate(userArray, currentOperation);
-          displayFirstNumber(userArray, firstNumber);
-          currentArrayPosition = 0;
+          userArray[3] = "=";
+          displayAll(userArray, firstNumber, secondNumber, resultNumber);
+          currentArrayPosition = 1;
           currentOperation = null;
           break;
         case "clear":
-          userArray = [0, 0];
-          displayFirstNumber(userArray, firstNumber);
-          currentArrayPosition = 0;
+          userArray = [0, 0, 0, ""];
+          userArray[3] = "";
+          displayAll(userArray, firstNumber, secondNumber, resultNumber);
+          currentArrayPosition = 1;
           currentOperation = null;
           break;
       }
       console.log(userArray);
-      console.log(currentOperation);
+      // console.log(currentOperation);
     });
   });
 });
 
+function displayAll(array, first, second, result) {
+  displayFirstNumber(array, first);
+  displaySecondNumber(array, second);
+  displayResultNumber(array, result);
+}
+
 function displayFirstNumber(array, first) {
-  if (array[1] != 0) first.textContent = array[1];
-  else first.textContent = array[0];
+  first.textContent = array[1] !== 0 ? array[1] : "0";
+}
+
+function displaySecondNumber(array, second) {
+  second.textContent = array[2];
+}
+
+function displayResultNumber(array, result) {
+  result.textContent = array[0];
 }
 
 function enterValue(array, n, position) {
   const num = parseInt(n, 10);
 
-  if (array[0] === 0 && array[1] === 0) array[0] = num;
-  else if (position === 0) array[0] = attachNumber(n, array[0]);
+  if (array[0] === 0 && array[1] === 0 && array[2] === 0) array[1] = num;
   else if (position === 1) array[1] = attachNumber(n, array[1]);
+  else if (position === 2) array[2] = attachNumber(n, array[2]);
 }
 
 function attachNumber(number, currentValue) {
@@ -129,22 +154,23 @@ function attachNumber(number, currentValue) {
 }
 
 function calculate(array, operation) {
-  if (array[0] != 0 && array[1] != 0) {
+  if (array[1] != 0 && array[2] != 0) {
     array[0] = operate(array, operation);
-    array[1] = 0;
+    array[1] = array[0];
+    array[2] = 0;
   }
 }
 
 function operate(array, operation) {
   switch (operation) {
     case "add":
-      return add(array[0], array[1]);
+      return add(array[1], array[2]);
     case "subtract":
-      return subtract(array[0], array[1]);
+      return subtract(array[1], array[2]);
     case "multiply":
-      return multiply(array[0], array[1]);
+      return multiply(array[1], array[2]);
     case "divide":
-      return divide(array[0], array[1]);
+      return divide(array[1], array[2]);
   }
 }
 
